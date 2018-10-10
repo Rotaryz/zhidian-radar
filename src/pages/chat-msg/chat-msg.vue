f9ae50fa9087a924b1723f8ce5beb888f05ff6a0
jtr <jtr354@foxmail.com>
2018-10-10 21:53:25 +0800

    接weixinjssdk-字段驼峰

--- src/pages/chat-msg/chat-msg.vue
+++ src/pages/chat-msg/chat-msg.vue
@@ -198,16 +198,17 @@
           }, 20)
         }
       })
-      Global.jssdkConfig().then((res) => {
-        if (res.error === ERR_OK) {
+      let url = window.location.href
+      Global.jssdkConfig({url}).then((res) => {
+        if (res.error === this.$ERR_OK) {
           res = res.data
           wx.config({
             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: res.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
-            timestamp: res.timestamp, // 必填，生成签名的时间戳
-            nonceStr: res.noncestr, // 必填，生成签名的随机串
-            signature: res.signature, // 必填，签名，见附录1
-            jsApiList: ['previewImage', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            appId: '' + res.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
+            timestamp: '' + res.timestamp, // 必填，生成签名的时间戳
+            nonceStr: '' + res.nonceStr, // 必填，生成签名的随机串
+            signature: '' + res.signature, // 必填，签名，见附录1
+            jsApiList: ['previewImage', 'scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
           })
         }
       })
