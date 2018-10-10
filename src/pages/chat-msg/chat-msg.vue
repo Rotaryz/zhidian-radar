062d278a12067cf0ff2b73b3fd965bd199472304
panranran <ran@jkweixin.com>
2018-10-10 18:09:43 +0800

    test

--- src/pages/chat-msg/chat-msg.vue
+++ src/pages/chat-msg/chat-msg.vue
@@ -234,17 +234,7 @@
         'setNowChat'
       ]),
       showPic(item) {
-        // wx.previewImage({urls: [item.url]})
-        wx.onMenuShareAppMessage({
-          title: 'test', // 分享标题
-          desc: '', // 分享描述
-          link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-          imgUrl: '', // 分享图标
-          type: '', // 分享类型,music、video或link，不填默认为link
-          dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-          success: function () { // 用户点击了分享后执行的回调函数
-          }
-        })
+        wx.previewImage({urls: [item.url]})
       },
       getQrCodeStatus() {
         Im.getCodeStatus().then(res => {
