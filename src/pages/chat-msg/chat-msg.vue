5f996aa863d40065f15653d76149946b153ccc3f
panranran <ran@jkweixin.com>
2018-10-10 18:07:39 +0800

    test

--- src/pages/chat-msg/chat-msg.vue
+++ src/pages/chat-msg/chat-msg.vue
@@ -207,7 +207,7 @@
             timestamp: res.timestamp, // 必填，生成签名的时间戳
             nonceStr: res.noncestr, // 必填，生成签名的随机串
             signature: res.signature, // 必填，签名，见附录1
-            jsApiList: ['previewImage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            jsApiList: ['previewImage', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
           })
         }
       })
@@ -234,7 +234,17 @@
         'setNowChat'
       ]),
       showPic(item) {
-        wx.previewImage({urls: [item.url]})
+        // wx.previewImage({urls: [item.url]})
+        wx.onMenuShareAppMessage({
+          title: 'test', // 分享标题
+          desc: '', // 分享描述
+          link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: '', // 分享图标
+          type: '', // 分享类型,music、video或link，不填默认为link
+          dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+          success: function () { // 用户点击了分享后执行的回调函数
+          }
+        })
       },
       getQrCodeStatus() {
         Im.getCodeStatus().then(res => {
