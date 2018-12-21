d01ff09ae6e8287b44ac286338f2a9a572ea84b0
jtr <jtr354@foxmail.com>
2018-12-21 11:59:28 +0800

    优化提示

--- src/utils/env-clear.js
+++ src/utils/env-clear.js
@@ -1,6 +1,6 @@
 import storage from 'storage-controller'
 import APP_CONFIG from './_app-config'
-const APP_ENV = process.env.NODE_ENV
+// const APP_ENV = process.env.NODE_ENV
 // 清理缓存
 function envClear() {
   const env = APP_CONFIG.env + '：' + APP_CONFIG.api
@@ -14,10 +14,10 @@ function envClear() {
   console.warn('环境：' + env)
 }
 // 检查生产环境是否配置正确
-function checkVersion() {
-  if (APP_CONFIG.env !== APP_ENV && APP_ENV === 'production') {
-    throw new Error('生产环境配置不一致')
-  }
-}
-checkVersion()
+// function checkVersion() {
+//   if (APP_CONFIG.env !== APP_ENV && APP_ENV === 'production') {
+//     throw new Error('生产环境配置不一致')
+//   }
+// }
+// checkVersion()
 envClear()
