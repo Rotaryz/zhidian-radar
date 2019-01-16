3332e3061cbcb6d9e34e9d16b26cd9abe5d12036
philly <346692921@qq.com>
2019-01-16 20:18:44 +0800

    调整

--- src/utils/_app-config.js
+++ src/utils/_app-config.js
@@ -1,7 +1,7 @@
-const version = ``
+const version = `/v1`
 export default {
   app: '赞播平台',
-  env: 'production',
-  api: 'https://zhidian-api.jkweixin.com' + version,
-  upload: 'https://zhidian-api.jkweixin.com' + version
+  env: 'release',
+  api: 'https://zhidian-api.jkweixin.net' + version,
+  upload: 'https://zhidian-api.jkweixin.net' + version
 }
