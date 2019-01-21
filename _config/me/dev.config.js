dcf6f6c2bc2788f4f60ec2f707a249aa6239d468
jtr <jtr354@foxmail.com>
2019-01-21 17:58:47 +0800

    修改ME域名

--- _config/me/dev.config.js
+++ _config/me/dev.config.js
@@ -3,9 +3,9 @@ const versionIm = ``
 export default {
   app: 'ME时代',
   env: 'dev',
-  api: 'https://zhidian-me-api.jerryf.cn' + version,
-  upload: 'https://zhidian-me-api.jerryf.cn' + version,
+  api: 'https://zhidian-me-era-api.jerryf.cn' + version,
+  upload: 'https://zhidian-me-era-api.jkweixin.net' + version,
   // mall: 'https://exchange-mall-api.jkweixin.net' + version,
-  oauthURI: 'https://zhidian-me-api.jkweixin.net',
+  oauthURI: 'https://zhidian-me-era-api.jkweixin.net',
   imRecord: 'https://data-analysis-api.jkweixin.net' + versionIm
 }
--- _config/me/release.config.js
+++ _config/me/release.config.js
@@ -6,6 +6,6 @@ export default {
   api: 'https://zhidian-me-era-api.jkweixin.net' + version,
   upload: 'https://zhidian-me-era-api.jkweixin.net' + version,
   // mall: 'https://exchange-mall-api.jkweixin.net' + version,
-  oauthURI: 'https://zhidian-me-api.jkweixin.net',
+  oauthURI: 'https://zhidian-me-era-api.jkweixin.net',
   imRecord: 'https://data-analysis-api.jkweixin.net' + versionIm
 }
