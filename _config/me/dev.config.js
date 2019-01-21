0dbb0fe6e396079090fdabf96f0d7e500dc1b273
jtr <jtr354@foxmail.com>
2019-01-21 17:31:23 +0800

    pc发版本号

--- _config/me/dev.config.js
+++ _config/me/dev.config.js
@@ -1,7 +1,11 @@
 const version = ``
+const versionIm = ``
 export default {
   app: 'ME时代',
   env: 'dev',
   api: 'https://zhidian-me-api.jerryf.cn' + version,
-  upload: 'https://zhidian-me-api.jerryf.cn' + version
+  upload: 'https://zhidian-me-api.jerryf.cn' + version,
+  // mall: 'https://exchange-mall-api.jkweixin.net' + version,
+  oauthURI: 'https://zhidian-me-api.jkweixin.net',
+  imRecord: 'https://data-analysis-api.jkweixin.net' + versionIm
 }
--- _config/me/production.config.js
+++ _config/me/production.config.js
@@ -1,7 +1,11 @@
 const version = ``
+const versionIm = ``
 export default {
   app: 'ME时代',
   env: 'production',
   api: 'https://zhidian-me-era-api.jkweixin.com' + version,
-  upload: 'https://zhidian-me-era-api.jkweixin.com' + version
+  upload: 'https://zhidian-me-era-api.jkweixin.com' + version,
+  // mall: 'https://exchange-mall-api.jkweixin.com' + version,
+  oauthURI: 'https://zhidian-me-api.jkweixin.com',
+  imRecord: 'https://data-analysis-api.jkweixin.com' + versionIm
 }
