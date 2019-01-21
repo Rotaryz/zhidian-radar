9409769452a67a3dc0dd47d8059bb251cba0a8b1
jtr <jtr354@foxmail.com>
2019-01-21 18:00:22 +0800

    修改ME域名

--- src/utils/_app-config.js
+++ src/utils/_app-config.js
@@ -6,10 +6,6 @@ export default {
   api: 'https://zhidian-me-era-api.jkweixin.net' + version,
   upload: 'https://zhidian-me-era-api.jkweixin.net' + version,
   // mall: 'https://exchange-mall-api.jkweixin.net' + version,
-<<<<<<< HEAD
-  oauthURI: 'https://zhidian-api.jkweixin.net',
-=======
   oauthURI: 'https://zhidian-me-era-api.jkweixin.net',
->>>>>>> fix-branch-me
   imRecord: 'https://data-analysis-api.jkweixin.net' + versionIm
 }
