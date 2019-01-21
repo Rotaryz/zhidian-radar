26f0d3cef5574fdde68fc9d82281407a4b63a3f3
jtr <jtr354@foxmail.com>
2019-01-21 18:07:29 +0800

    修改ME域名

--- _config/me/dev.config.js
+++ _config/me/dev.config.js
@@ -5,7 +5,7 @@ export default {
   env: 'dev',
   api: 'https://zhidian-me-era-api.jerryf.cn' + version,
   upload: 'https://zhidian-me-era-api.jkweixin.net' + version,
-  // mall: 'https://exchange-mall-api.jkweixin.net' + version,
+  mall: 'https://exchange-mall-api.jkweixin.net' + version,
   oauthURI: 'https://zhidian-me-era-api.jkweixin.net',
   imRecord: 'https://data-analysis-api.jkweixin.net' + versionIm
 }
