d23ea2e87f84322022da6a212de5babe07cb85d1
jtr <jtr354@foxmail.com>
2019-01-21 18:05:36 +0800

    sad

--- _config/me/production.config.js
+++ _config/me/production.config.js
@@ -6,10 +6,5 @@ export default {
   api: 'https://zhidian-me-era-api.jkweixin.com' + version,
   upload: 'https://zhidian-me-era-api.jkweixin.com' + version,
   // mall: 'https://exchange-mall-api.jkweixin.com' + version,
-<<<<<<< HEAD
-  oauthURI: 'https://zhidian-api.jkweixin.com',
-=======
-  oauthURI: 'https://zhidian-me-era-api.jkweixin.com',
->>>>>>> fix-branch-me
   imRecord: 'https://data-analysis-api.jkweixin.com' + versionIm
 }
