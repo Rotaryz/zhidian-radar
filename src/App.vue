6dfc48e7e746cd261e508038abaf1b715438db79
jtr <jtr354@foxmail.com>
2019-01-22 16:42:58 +0800

    调试音频

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
     <router-view />
+    <audio ref="musicAudio">
+      <source src="./test.mp3" type="audio/mpeg">
+    </audio>
+    <button @click="play" style="z-index: 9999;height: 100px;width: 200px;position: fixed; background: red">音乐播放</button>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   export default {
-    name: 'App'
+    name: 'App',
+    methods: {
+      play() {
+        this.$refs.musicAudio.play()
+      }
+    }
   }
 </script>
 
