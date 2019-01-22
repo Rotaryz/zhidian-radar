d646965b86141d2eb698f7018ff97d250b2a43a6
jtr <jtr354@foxmail.com>
2019-01-22 15:35:24 +0800

    调试音频

--- src/utils/audio-plugin.js
+++ src/utils/audio-plugin.js
@@ -1,24 +1,42 @@
-window.$nowTime = 0 // 记录播放的开始时刻
-function _createAudio() {
-  window.$audio = new Audio()
-  window.$audio.src = '/static/qq.mp3'
-  window.$audio.addEventListener('loadedmetadata', () => {
-    // alert(window.$audio.duration)
-    // console.log(this)
+// window.$nowTime = 0 // 记录播放的开始时刻
+// function _createAudio() {
+//   window.$audio = document.querySelector('#musicAudio')
+//   window.$audio.addEventListener('loadedmetadata', () => {
+//     // alert(window.$audio.duration)
+//     console.log(window.$audio.duration)
+//   })
+// }
+// _createAudio()
+//
+// window.$playAudio = function () {
+//   // let duration = this.$audio.duration * 1000
+//   // if (!duration) return
+//   // if (Date.now() - this.$nowTime < duration) return
+//   // this.$nowTime = Date.now
+//   // document.getElementById('musicAudio').play()
+//   document.addEventListener('WeixinJSBridgeReady', function () {
+//     let media = document.getElementById('musicAudio').play()
+//     media.src = '/static/qq.mp3'
+//     media.play()
+//   }, false)
+// }
+//
+// console.log(window.WeixinJSBridge)
+import wx from 'weixin-js-sdk'
+function autoPlayAudio1() {
+  wx.config({
+    // 配置信息, 即使不正确也能使用 wx.ready
+    debug: false,
+    appId: '',
+    timestamp: 1,
+    nonceStr: '',
+    signature: '',
+    jsApiList: []
   })
 }
-_createAudio()
-
-window.$playAudio = function () {
-  alert(window.$audio.play)
-  if (!this.$audio) return
-  // let duration = this.$audio.duration * 1000
-  // if (!duration) return
-  // if (Date.now() - this.$nowTime < duration) return
-  // this.$nowTime = Date.now
-  document.addEventListener('WeixinJSBridgeReady', function () {
-    window.$audio && window.$audio.play()
-    // document.getElementById('audio').play();
-    // document.getElementById('video').play();
-  }, false)
+autoPlayAudio1()
+window.$playAudio = () => {
+  wx.ready(function() {
+    document.getElementById('musicAudio').play()
+  })
 }
