9dbd02997729030a701702ff775a040bc11368d9
jtr <jtr354@foxmail.com>
2019-02-12 10:36:57 +0800

    添加营销页面

---
+++ src/pages/n-marketing/n-marketing.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="n-marketing">
+    n-marketing
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const PAGE_NAME = 'N_MARKETING'
+
+  export default {
+    name: PAGE_NAME,
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import '~common/stylus/mixin'
+
+  .n-marketing
+    width: 100%
+</style>
