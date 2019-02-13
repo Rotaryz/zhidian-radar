c594365f16ae72faa25bb514b577f278efaa00e3
jtr <jtr354@foxmail.com>
2019-02-13 19:02:55 +0800

    合并代码

--- src/pages/n-marketing/n-marketing.vue
+++ src/pages/n-marketing/n-marketing.vue
@@ -108,12 +108,12 @@
             background: #FFFFFF
             border-radius: 4px
     .container
-      width: 100vw
-      height: 100vh
+      width: 100%
+      height: 100%
       overflow :hidden
       .big-container
-        width: 200vw
-        height: 100vh
+        width: 200%
+        height: 100%
         display :flex
         flex-direction :row
         transition: transform 0.6s
