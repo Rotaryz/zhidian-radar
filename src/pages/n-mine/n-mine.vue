9c3ec60311cc4f020fbfa6ba15ec38ff3d2cb0ee
jtr <jtr354@foxmail.com>
2019-02-21 17:31:29 +0800

    test qr=code

--- src/pages/n-mine/n-mine.vue
+++ src/pages/n-mine/n-mine.vue
@@ -168,6 +168,10 @@
     right: 0
     bottom: 0
     top: 0
+    overflow :hidden
+    display :flex
+    align-items :center
+    justify-content :center
     .center-box
       width: 200px
       height: 200px
