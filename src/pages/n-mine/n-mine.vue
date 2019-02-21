8dc6f1d5e218852c1914e8ed7d8765bca73a54b4
philly <346692921@qq.com>
2019-02-21 16:40:40 +0800

    测试二维码

--- src/pages/n-mine/n-mine.vue
+++ src/pages/n-mine/n-mine.vue
@@ -175,9 +175,13 @@
       left: 50%
       top: 50%
       transform: translate(-50%, -50%)
+      overflow: hidden
       .center-img
         width: 100%
         height: 100%
+        padding: 1000px
+        margin: -1000px
+
   div
     box-sizing: border-box
     -moz-box-sizing: border-box
