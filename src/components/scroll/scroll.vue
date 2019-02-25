1bd02c0bbdf91afc02bc8b9866fb98569e22b8d7
jtr <jtr354@foxmail.com>
2019-02-25 11:04:23 +0800

    优化scroll

--- src/components/scroll/scroll.vue
+++ src/components/scroll/scroll.vue
@@ -189,7 +189,7 @@
           // bounceTime: 500,
           // swipeTime: 500,
           // swipeBounceTime: 500,
-          deceleration: 0.0015 * 2
+          deceleration: 0.0015 * 1.5
         }
         this.scroll = new BScroll(this.$refs.wrapper, options)
         if (this.listenScroll) {
