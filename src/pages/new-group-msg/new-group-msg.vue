1bff85ccf64271fe52bc8935fd6dd1856d4a6444
philly <346692921@qq.com>
2019-02-27 09:25:47 +0800

    群发优惠券样式修改

--- src/pages/new-group-msg/new-group-msg.vue
+++ src/pages/new-group-msg/new-group-msg.vue
@@ -60,7 +60,7 @@
                     <img src="./pic-coupon_bg@2x.png" class="coupon-bc">
                     <div class="coupon-container">
                       <div class="coupon-left">
-                        <div class="left-money">
+                        <div class="left-money" v-if="item.coupon_type == 3">
                           <span class="money-icon">¥</span>
                           <span class="money-txt">{{item.coupon_num}}</span>
                         </div>
