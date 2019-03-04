47703609934de96603bccaf59d764c3697741713
jtr <jtr354@foxmail.com>
2019-03-04 18:46:32 +0800

    auto publish

--- src/utils/_app-config.js
+++ src/utils/_app-config.js
@@ -3,10 +3,10 @@ const versionIm = ``
 export default {
   app: '赞播平台',
   appKey: 'platform',
-  env: 'release',
-  api: 'https://zhidian-api.jkweixin.net' + version,
-  upload: 'https://zhidian-api.jkweixin.net' + version,
-  mall: 'https://exchange-mall-api.jkweixin.net' + version,
-  oauthURI: 'https://zhidian-api.jkweixin.net',
-  imRecord: 'https://data-analysis-api.jkweixin.net' + versionIm
+  env: 'production',
+  api: 'https://zhidian-api.jkweixin.com' + version,
+  upload: 'https://zhidian-api.jkweixin.com' + version,
+  mall: 'https://exchange-mall-api.jkweixin.com' + version,
+  oauthURI: 'https://zhidian-api.jkweixin.com',
+  imRecord: 'https://data-analysis-api.jkweixin.com' + versionIm
 }
